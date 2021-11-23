e498108f0d6a651c53db464d3bc0c55d0c962b56
gursimran29 <gursimran2901@gmail.com>
2021-11-23 23:11:26 +0530

    home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,164 +1,89 @@
 import "./Home.css";
-import person1 from "./Image/person1-nobg.png";
+import hand from "./Image/hand.png";
+import neuro from "./Image/neuro.png";
+import doc from "./Image/doctor.png";
+import patient from "./Image/hpatient.png";
+
 import { Container, Accordion, Image } from "react-bootstrap";
 
 function Home() {
   return (
     <div className="Home">
       <Container>
-        <div class="p-5 mb-4 bg-light rounded-3">
-          <div class="container-fluid py-5">
-            <h1 class="display-5 fw-bold">Neurora</h1>
-            <p class="col-md-8 fs-4">
-              Using a series of utilities, you can create this Lorem ipsum dolor
-              sit amet, consectetur adipiscing elit. Vestibulum consequat
-              scelerisque elit sit amet consequat.
-            </p>
-            <div class="learn-more">
-              Click
-              <a href="#faq" class="learn-more-btn">
-                here
-              </a>
-              to learn more
+        <div className="p-5 mb-4 ">
+          <div className="container-fluid py-5">
+            <h1 className="display-5 fw-bold">Neurora</h1>
+            <div className="row">
+              <div className="col-sm-8">
+                <p className="firstt">
+                  We strive to provide you with the best in quality, reliable and accessible MDD Diagnosis using AI
+                </p>
+                <p className="textt"> We believe providing more access to reliable, efficient and fast Depression diagnosis can benefit humankind.</p>
+              </div>
+              <div className="col-sm-4">
+                <img src={hand} className="img1" />
+              </div>
+
+            </div>
+            <hr style={{ borderBottom: "0.35px solid #c2cccc", width: "100%", marginBottom: "37px", opacity: "35%" }} />
+
+            <div className="row">
+
+              <div className="col-sm-5">
+                <img src={neuro} className="img2" />
+              </div>
+              <div className="col-sm-7">
+
+                <p className="textt"> We help neurology practices achieve peak performance, resulting in premier patient care and positive profits to better serve your customers and patients.
+                </p>
+              </div>
+
+            </div>
+            <hr style={{ borderBottom: "0.35px solid #c2cccc", width: "100%", marginTop: "54px", marginBottom: "39px", opacity: "35%" }} />
+            <div className="row">
+              <div className="col-sm-9">
+                <div className="doctor">
+                  <h3>For Doctors-</h3>
+
+                  <p>
+                    - get fast and reliable EEG results to deliver superior patient <br></br>    outcomes<br></br>
+                    - keep a track of all your patients and their medical histories all <br></br>at one place
+                  </p>
+                </div>
+              </div>
+              <div className="col-sm-3">
+                <img src={doc} className="img2" />
+              </div>
+
+
+
+            </div>
+            <hr style={{ borderBottom: "0.35px solid #c2cccc", width: "100%", marginTop: "54px", marginBottom: "37px", opacity: "35%" }} />
+            <div className="row">
+
+              <div className="col-sm-5">
+                <img src={patient} className="img4" />
+              </div>
+
+              <div className="col-sm-7">
+                <div className="doctor">
+                  <h3>For Patients-</h3>
+
+                  <p>
+                    - get trustworthy and faster results <br></br>
+                    - have all your reports stored digitally at one place!
+
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="endline">
+              <p ><i> Neurora - an easy and simple to use interface providing fast and trustworthy results</i></p>
             </div>
           </div>
         </div>
 
-        <div class="container">
-          <div class="row">
-            <div class="col col1">
-              <div class="home-card-content">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="50"
-                  height="50"
-                  color="#FFFFFF"
-                  fill="currentColor"
-                  class="bi bi-clock "
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
-                  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
-                </svg>
-                <br />
-                <div class=" home-card-svg-title">
-                  <span class=" home-card-svg-title-text">WORKING HOURS</span>
-                  <p class="home-card-svg-title-para">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Vestibulum consequat scelerisque elit sit amet consequat.{" "}
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="col col2">
-              <div class="home-card-content">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="50"
-                  height="50"
-                  color="#FFFFFF"
-                  fill="currentColor"
-                  class="bi bi-envelope-open "
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M8.47 1.318a1 1 0 0 0-.94 0l-6 3.2A1 1 0 0 0 1 5.4v.817l5.75 3.45L8 8.917l1.25.75L15 6.217V5.4a1 1 0 0 0-.53-.882l-6-3.2ZM15 7.383l-4.778 2.867L15 13.117V7.383Zm-.035 6.88L8 10.082l-6.965 4.18A1 1 0 0 0 2 15h12a1 1 0 0 0 .965-.738ZM1 13.116l4.778-2.867L1 7.383v5.734ZM7.059.435a2 2 0 0 1 1.882 0l6 3.2A2 2 0 0 1 16 5.4V14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V5.4a2 2 0 0 1 1.059-1.765l6-3.2Z"
-                  />
-                </svg>
-                <br />
-                <div class=" home-card-svg-title">
-                  <span class=" home-card-svg-title-text">GET IN TOUCH</span>
-                  <p class="home-card-svg-title-para">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Vestibulum consequat scelerisque elit sit amet consequat.{" "}
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="col col3">
-              <div class="home-card-content">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="50"
-                  height="50"
-                  color="#FFFFFF"
-                  fill="currentColor"
-                  class="bi bi-bag-plus "
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
-                  />
-                  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                </svg>
-                <br />
-                <div class=" home-card-svg-title">
-                  <span class="home-card-svg-title-text">QUICK HELP</span>
-                  <p class="home-card-svg-title-para">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Vestibulum consequat scelerisque elit sit amet consequat.{" "}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row about-clinic">
-          <div class="col-4">
-            <Image src={person1} fluid />
-          </div>
-          <div class="col-8 about-clinic-content" id="faq">
-            <span class="about-clinic-content-heading">About Our Clinic</span>
-            <p class="about-clinic-content-byline">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Vestibulum consequat scelerisque elit sit
-            </p>
-            <div class="clinic-acc">
-              <Accordion>
-                <Accordion.Item eventKey="0">
-                  <Accordion.Header>Accordion Item #1</Accordion.Header>
-                  <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
-                  </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="1">
-                  <Accordion.Header>Accordion Item #2</Accordion.Header>
-                  <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis
-                  </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="2">
-                  <Accordion.Header>Accordion Item #3</Accordion.Header>
-                  <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis
-                  </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="3">
-                  <Accordion.Header>Accordion Item #4</Accordion.Header>
-                  <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore
-                  </Accordion.Body>
-                </Accordion.Item>
-              </Accordion>
-            </div>
-          </div>
-        </div>
       </Container>
-      <br />
     </div>
   );
 }
